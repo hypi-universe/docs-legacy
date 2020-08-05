fb7e42a9e55e29420a84e93615e9609b80f2c865
Damion Robinson <damion.robinson@hypi.io>
2020-08-05 09:26:12 +0100

    updated the global styles to improve the readability of the docs

--- src/components/mdxComponents/code.js
+++ src/components/mdxComponents/code.js
@@ -3,7 +3,7 @@ import styled from "react-emotion";
 const Code = styled('code')`
   background: #f9f7fb;
   border: 1px solid #ede7f3;
-  border-radius: 4px;
+  border-radius: 0;
   padding: 2px 6px;
   font-size: 0.9375em;
 `;
